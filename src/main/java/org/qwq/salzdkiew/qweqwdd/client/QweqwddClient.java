f7de8202a78613f7f0502c61ddaa2098ed7c4369
SaltDirt.F <109417431+SaltDirt@users.noreply.github.com>
2023-07-07 16:36:47 +0800

    Add files via upload

---
+++ src/main/java/org/qwq/salzdkiew/qweqwdd/client/QweqwddClient.java
@@ -0,0 +1,15 @@
+package org.qwq.salzdkiew.qweqwdd.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.qwq.salzdkiew.qweqwdd.Qweqwdd;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+public class QweqwddClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
